cb04720033c23c41ecb3b108166366029c7c398c
Jess <jess.m.stanton@gmail.com>
2011-03-23 16:32:28 -0700

    first stuff. no big deal, README and setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='toolbox',
+    version='0.1-snapshot',
+    packages=[
+        'toolbox',
+    ],
+    namespace_packages = ['toolbox'],
+    install_requires=[
+    ],
+    
+    zip_safe=False,
+
+    description = "Toolbox",
+)
+
